1650090e901001a5616866c46c1e310fdfca2d70
Kaniel <g.orwell@hip.hosting>
2026-09-07 15:20:05 +0300

    Fix Wordpess installation script

--- wordpress/install.sh
+++ wordpress/install.sh
@@ -16,37 +16,11 @@ echo "[1/5] Installing Docker..."
 
 apt-get update
 
-apt-get install -y \
-    ca-certificates \
-    curl \
-    openssl
+apt-get install -y curl openssl
 
-install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://get.docker.com | sh
 
-curl -fsSL \
-    https://download.docker.com/linux/ubuntu/gpg \
-    -o /etc/apt/keyrings/docker.asc
-
-chmod a+r /etc/apt/keyrings/docker.asc
-
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] \
-  https://download.docker.com/linux/ubuntu \
-  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" \
-  > /etc/apt/sources.list.d/docker.list
-
-apt-get update
-
-apt-get install -y \
-    docker-ce \
-    docker-ce-cli \
-    containerd.io \
-    docker-buildx-plugin \
-    docker-compose-plugin
-
-systemctl enable docker
-systemctl start docker
-
+systemctl enable --now docker
 
 # --------------------------------------------------
 # Create directories
