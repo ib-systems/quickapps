6ea7f23c7aaf9e666928fed6fc9f2170a2d40826
Kaniel <g.orwell@hip.hosting>
2026-09-25 18:58:08 +0300

    fix

--- chatwoot/install.sh
+++ chatwoot/install.sh
@@ -96,7 +96,8 @@ EOF
 
 docker compose up -d postgres redis
 
-until docker compose exec -T postgres pg_isready -U postgres -d chatwoot; do
+until docker compose exec -T postgres pg_isready -U postgres -d chatwoot >/dev/null 2>&1
+do
     echo "Waiting for PostgreSQL..."
     sleep 5
 done
