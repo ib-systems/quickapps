e9b30375aba714014e12982b5d59762b734b2327
Kaniel <g.orwell@hip.hosting>
2026-09-25 20:16:54 +0300

    test

--- chatwoot/install.sh
+++ chatwoot/install.sh
@@ -96,13 +96,13 @@ EOF
 
 docker compose up -d postgres redis
 
-sleep 30
+sleep 400
 
-while ! docker compose exec -T postgres pg_isready -U postgres -d chatwoot >/dev/null 2>&1
-do
-    echo "Waiting for PostgreSQL..."
-    sleep 5
-done
+#while ! docker compose exec -T postgres pg_isready -U postgres -d chatwoot >/dev/null 2>&1
+#do
+ #   echo "Waiting for PostgreSQL..."
+  #  sleep 5
+#done
 
 
 docker compose run --rm --entrypoint "" rails bundle exec rails db:chatwoot_prepare
